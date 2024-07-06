a34b6259b050de256fcb9bd93a0f2ef062b26283
klyn <klyn@gmail.com>
2024-07-06 09:26:09 -0300

    modified:   .local/bin/eww

--- .config/eww/scripts/python/material-colors.py
+++ .config/eww/scripts/python/material-colors.py
@@ -56,7 +56,7 @@ def get_colors_from_img(image, scheme):
 
 
 def get_colors_from_color(color, scheme):
-    theme = themeFromSourceColor(color)
+    theme = themeFromSourceColor(argbFromHex(color))
     colorscheme = theme.get('schemes').get(scheme)
     
     return get_colors(colorscheme)
